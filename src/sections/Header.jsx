d8a9f1bf193d8836a5e58f2a68a03d2d41784bd1
Neshad <djneshad@gmail.com>
2025-05-01 20:39:52 +0600

    features done

--- src/sections/Header.jsx
+++ src/sections/Header.jsx
@@ -5,14 +5,7 @@ import { Link as LinkScroll } from 'react-scroll';
 
 
 
-const NavLink = ({ title }) => (
-  <LinkScroll
-    className='base-bold text-p4 uppercase transition-colors duration-500
-    cursor-pointer hover:text-p1 max-lg:my-4 max-lg:h5'
-  >
-    {title}
-  </LinkScroll>
-)
+
 
 
 
@@ -31,6 +24,15 @@ const Header = () => {
       window.removeEventListener('scroll', handleScroll);
     };
   }, [])
+
+  const NavLink = ({ title }) => (
+    <LinkScroll onClick={() => setIsOpen(false)} to={title} offset={-100} spy smooth activeClass='nav-active'
+      className='base-bold text-p4 uppercase transition-colors duration-500
+      cursor-pointer hover:text-p1 max-lg:my-4 max-lg:h5'
+    >
+      {title}
+    </LinkScroll>
+  );
 
   return (
     <header className={clsx('fixed top-0 left-0 z-50 w-full py-10 transition-all duration-500 max-lg:py-4', hasScrolled && 'py-2 bg-black-100 backdrop-blur-[8px]')}>
